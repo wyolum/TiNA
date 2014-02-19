eb0166c4a1b330974bf22807304f374ae9647ea2
= <=>
2014-02-18 20:58:41 -0500

    setter.py now functions as a remote interface

--- arduino/libraries/TiNA/examples/Clock/setter.py
+++ arduino/libraries/TiNA/examples/Clock/setter.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from Tkinter import *
-from C3jr_interface import *
+from interface import *
 connect(getSerialports()[0])
 
 def do_set(*args, **kw):
@@ -37,8 +37,6 @@ def curry(func, arg):
 def reconnect(port):
     print(port)
     connect(serialport=port)
-def do_scroll():
-    scroll_msg("SCROLL");
 
 tk = Tk()
 f = Frame(tk)
@@ -48,13 +46,24 @@ jr_time_l.grid(row=0, column=1)
 Button(f, text="Get!", command=do_get).grid(row=1, column=0)
 pc_time_l = Label(f, width=35, text="PC Time:", anchor=W, font='Courier')
 pc_time_l.grid(row=1, column=1)
-Button(f, text="SCROLL", command=do_scroll).grid(row=2, column = 0)
+f.grid(column=1, row=1)
 
 gmt_var = DoubleVar(f)
 gmt_var.set(-4) # default value
 
 w = OptionMenu(f, gmt_var, *range(-12, 12, 1))
 w.grid(row=2, column=0)
+
+
+keypad = Frame(tk)
+Button(keypad, text="UP", command=press_UP).grid(row=1, column=2)
+Button(keypad, text="LEFT", command=press_LEFT).grid(row=2, column=1)
+Button(keypad, text="OK", command=press_MIDDLE).grid(row=2, column=2)
+Button(keypad, text="RIGHT", command=press_RIGHT).grid(row=2, column=3)
+Button(keypad, text="DOWN", command=press_DOWN).grid(row=3, column=2)
+Button(keypad, text="Reset", command=press_RESET).grid(row=4, column=4)
+
+keypad.grid(row=2, column=1)
 
 
 menubar = Menu(tk)
@@ -66,5 +75,4 @@ menubar.add_cascade(label="USB", menu=usbmenu)
 tk.config(menu=menubar)
 
 
-f.pack()
 tk.mainloop()
